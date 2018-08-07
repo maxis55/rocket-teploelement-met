9e15914ac19e8c5290362cd92e3f2e1e6dec42dd
Nicolay_Petrovich <nicolaypetrovich@icloud.com>
2018-08-07 12:28:51 +0300

    Base Config

--- config/web.php
+++ config/web.php
@@ -14,7 +14,8 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'jdnfojsfjosjfkosjkofsjkofjj3jojoiu',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +44,13 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '<action>' => 'site/<action>',
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
