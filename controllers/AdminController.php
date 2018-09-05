152c4c5e11e4c351a559c91d4ae30d21af6d26e0
maxis55 <maxis.55.m@gmail.com>
2018-09-05 12:56:28 +0300

    Added login form

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -9,6 +9,8 @@
 namespace app\controllers;
 
 
+use app\models\LoginForm;
+use Yii;
 use yii\web\Controller;
 
 class AdminController extends Controller
@@ -23,5 +25,20 @@ class AdminController extends Controller
     {
 
         return $this->render('index');
+    }
+
+    public function actionLogin()
+    {
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+
     }
 }
--- views/layouts/admin/main.php
+++ views/layouts/admin/main.php
@@ -16,11 +16,6 @@ if (Yii::$app->controller->action->id === 'login') {
     );
 } else {
 
-    if (class_exists('backend\assets\AppAsset')) {
-        backend\assets\AppAsset::register($this);
-    } else {
-        app\assets\AppAsset::register($this);
-    }
 
     dmstr\web\AdminLteAsset::register($this);
 
