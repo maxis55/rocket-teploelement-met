445c0569344e7764f299540169ad8d40a0ce9246
maxis55 <maxis.55.m@gmail.com>
2018-09-05 12:18:56 +0300

    Added imagine plugin via composer

--- vendor/yiisoft/extensions.php
+++ vendor/yiisoft/extensions.php
@@ -75,4 +75,13 @@ return array (
       '@dmstr' => $vendorDir . '/dmstr/yii2-adminlte-asset',
     ),
   ),
+  'yiisoft/yii2-imagine' => 
+  array (
+    'name' => 'yiisoft/yii2-imagine',
+    'version' => '2.1.1.0',
+    'alias' => 
+    array (
+      '@yii/imagine' => $vendorDir . '/yiisoft/yii2-imagine/src',
+    ),
+  ),
 );
