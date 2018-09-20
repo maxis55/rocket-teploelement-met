69cd86890f58023007532e6caca89665070301f5
Unknown <danilderevyanchenko@gmail.com>
2018-09-20 17:52:29 +0300

    return of db

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=teplo2',
-    'username' => 'teplo2',
-    'password' => '123456',
+    'dsn' => 'mysql:host=rocket01.mysql.tools;dbname=rocket01_teploel',
+    'username' => 'rocket01_teploel',
+    'password' => 'b8jdbet3',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
