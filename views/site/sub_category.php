d6bef351fe0fb19010e2e1e743c3c73a94383567
maxis55 <maxis.55.m@gmail.com>
2018-09-27 12:20:52 +0300

    Check for missing file on single subcategory page

--- views/site/sub_category.php
+++ views/site/sub_category.php
@@ -8,7 +8,7 @@ use yii\helpers\Url;
 /** @var string $category_slug */
 /** @var string $subcategory_slug */
 /** @var string $subsubcategory_slug */
-$content=json_decode($current_category->content);
+$content = json_decode($current_category->content);
 ?>
 
 
@@ -22,13 +22,16 @@ $content=json_decode($current_category->content);
                 <div class="inner_description_flex">
                     <div class="inner_description_img">
                         <div class="inner_description_img_box">
-
-                            <img src="<?= $current_category->media->getImageOfSize(); ?>" alt="">
+                            <?php
+                            $tempMedia = $current_category->media;
+                            if (null != $tempMedia):?>
+                                <img src="<?= $current_category->media->getImageOfSize(); ?>" alt="">
+                            <?php endif; ?>
                         </div>
                     </div>
                     <div class="inner_description_text">
                         <h2 class="title1"><span><?= $current_category->name; ?></span></h2>
-                        <p><?=$current_category->shortdesc;?></p>
+                        <p><?= $current_category->shortdesc; ?></p>
                         <div class="inner_description_box">
                             <a href="#" class="add_btn modal_btn" data-modal="request">Получить цены</a>
                             <span class="inner_desc_text">Рассчитаем вашу заявку в течении часа</span>
@@ -36,16 +39,16 @@ $content=json_decode($current_category->content);
                     </div>
                 </div>
                 <div class="inner_description_app">
-                    <?=$content[0];?>
+                    <?= $content[0]; ?>
                 </div>
             </div>
             <div class="inner_feature">
                 <div class="inner_feature_flex">
                     <div class="inner_feature1">
-                        <?=$content[1];?>
+                        <?= $content[1]; ?>
                     </div>
                     <div class="inner_feature2">
-                        <?=$content[2];?>
+                        <?= $content[2]; ?>
                         <div class="inner_description_box">
                             <a href="#" class="add_btn modal_btn" data-modal="request">Получить цены</a>
                             <span class="inner_desc_text">Рассчитаем вашу заявку в течении часа</span>
@@ -55,12 +58,12 @@ $content=json_decode($current_category->content);
                 </div>
             </div>
             <div class="inner_offers">
-                <?= $this->render('products_in_grid_template.php',[
-                        'dataProvider'=>$dataProvider
+                <?= $this->render('products_in_grid_template.php', [
+                    'dataProvider' => $dataProvider
                 ]) ?>
             </div>
 
-            
+
         </div>
     </div>
 </div>
